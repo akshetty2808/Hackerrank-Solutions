b36e65df477f825d334945fdee1132cf0d7c116e
akshetty2808 <93930947+akshetty2808@users.noreply.github.com>
2023-01-27 10:22:32 +0530

    Add files via upload

---
+++ Vector-sort.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int x,n;
+    vector<int>v;
+    int size = v.size();
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>x;
+        v.push_back(x);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i=0; i<n; i++){
+        cout<<v[i]<<" ";
+    }
+    
+    return 0;
+}
